315affa62555df6ba9ea513b0d45a70b1e3c8d47
Ragul <ragulvishwa42@gmail.com>
2025-02-14 17:42:19 +0530

    footer

--- app/components/footer/page.tsx
+++ app/components/footer/page.tsx
@@ -1,8 +1,15 @@
 'use client'
 const Footer = () => {
+    const year = new Date().getFullYear();
     return (
-        <footer>
-            Footer
+        <footer className="w-full border-t-2 px-5 md:px-16 ">
+            <div className="flex items-center text-[clamp(0.5rem,0.8vw,1rem)] p-5 justify-between">
+                <div>© {year} Inspace. All rights reserved.</div>
+                <div className="flex ">
+                    <div>Terms of Service</div>
+                    <div>Privacy Policy</div>
+                </div>
+            </div>
         </footer>
     );
 }
